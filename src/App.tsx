0d824a18eac670a5de3d98bf7a65cec6459d3ee0
euni00 <euneee2000@naver.com>
2023-09-18 20:29:05 +0900

    create header & footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,43 @@ import { useRef, useState } from "react";
 const DivStyle = styled.div`
   display: flex;
 
+  .container {
+    width: 100%;
+    margin: 0 auto;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+  }
+
+  nav {
+    float: right;
+  }
+
+  header {
+    width: 100%;
+    height: 100px;
+    display: flex;
+  }
+
+  header div {
+    display: flex;
+  }
+
+  .musicBox {
+    width: 100%;
+    display: flex;
+    padding: 50px;
+  }
+
   button {
-    width: 200px;
+    width: 300px;
+    height: 300px;
     display: flex;
     justify-content: center;
     align-items: center;
+    margin: 50px;
   }
 
   .music1 {
@@ -23,6 +55,10 @@ const DivStyle = styled.div`
 
   .music3 {
     background-color: #e75c49;
+  }
+
+  .footer {
+    width: 100%;
   }
 `;
 
@@ -58,30 +94,51 @@ function App() {
 
   return (
     <DivStyle>
-      <h1>
-        <a href="https://www.youtube.com/">music React</a>
-      </h1>
-      <div>{index}</div>
-      <button class="music1" onClick={changeLink1}>
-        music1
-      </button>
-      <button class="music2" onClick={changeLink2}>
-        music2
-      </button>
-      <button class="music3" onClick={changeLink3}>
-        music3
-      </button>
+      <div class="container">
+        <header class="header">
+          <h1>
+            <a href="https://www.youtube.com/">music React</a>
+          </h1>
 
-      <iframe
-        ref={player}
-        width="560"
-        height="315"
-        src={`https://www.youtube.com/embed/${link}&autoplay=1`}
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
-      ></iframe>
+          <nav>
+            <div>kpop</div>
+            <div>pop song</div>
+            <div>Jpop</div>
+            <div>dance</div>
+          </nav>
+        </header>
+
+        <div>{index}</div>
+
+        <div class="musicBox">
+          <button class="music1" onClick={changeLink1}>
+            music1
+          </button>
+          <button class="music2" onClick={changeLink2}>
+            music2
+          </button>
+          <button class="music3" onClick={changeLink3}>
+            music3
+          </button>
+        </div>
+
+        <iframe
+          ref={player}
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${link}&autoplay=1`}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+
+        <footer class="footer">
+          <div>permium subscribe</div>
+          <div>custormer sercive center</div>
+          <div>address</div>
+        </footer>
+      </div>
     </DivStyle>
   );
 }
